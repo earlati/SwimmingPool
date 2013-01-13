f0736cc7c7b49393e8070cf56653eec684c0af6a
Enzo <enzo.arlati@libero.it>
2013-01-13 22:27:51 +0100

    add-file2git.sh : work from different directory

--- add-file2git.sh
+++ add-file2git.sh
@@ -3,8 +3,12 @@
 # FILE: add-file2git.sh 1.003 11.07.2011
 # ======================================
 
+dir=$( dirname $0 )
 
+git add $0
 
+(
+cd "$dir" 
 git add geany.project
 git add appunti.swimmingpool.txt
 
@@ -23,5 +27,5 @@ find . -type f -name "*.css" -exec git add {} \;
 find . -type f -name "*.js" -exec git add {} \;
 find . -type f -name "*.html" -exec git add {} \;
 
-
+)
 
